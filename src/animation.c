7976c1b6f0bee9da21e32ba40ef52b1bc1a5f070
Kaikou Loic Degni <>
2024-12-01 23:33:26 -0500

    Amelioration de la animation_obstacle
    Les obstacles bougent en meme temps

--- src/animation.c
+++ src/animation.c
@@ -21,28 +21,19 @@ void obstacle_animation(file *Obstacles, float delta_time) {
    if (Obstacles == NULL || Obstacles->first == NULL) {
       return;     
    }
-   /**
-   int distance = (int)(delta_time * OBSTACLES_SPEED_EASY);
-   
-   if (Obstacles->first->rayon <= distance) {
-      Obstacles->first->rayon = 0;
-      file_update(Obstacles); 
-   } else {
-      Obstacles->first->rayon -= distance; 
-   }
-   */
    int distance = (int)(delta_time*OBSTACLES_SPEED_EASY);
 
-   for(obstacle *current = Obstacles->first; current !=NULL; current = current->next){
+   for(obstacle *current = Obstacles->first; current !=NULL; current = current->prev){
       if(current->rayon < distance){
          current->rayon = 0;
       }else{
          current->rayon -=distance;
+         printf("%d %d\n", current->rayon, current->cadran);
       }
       // Ajouter la verif de la position cadran du joueur 
    }
 
    while(Obstacles->first->rayon == 0)
       file_update(Obstacles);
-   printf("%d\n", distance);
+   //printf("%d\n", distance);
 }
