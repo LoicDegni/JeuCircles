7d8f15dd4f883ea197742b97fdc1994386dfc9a9
Kaikou Loic Degni <>
2024-12-05 22:16:25 -0500

    Ajout de fonctions de validation de pointeur dans obstacle

--- src/obstacle.c
+++ src/obstacle.c
@@ -9,57 +9,47 @@ file *file_obstacle_initialize() {
 }
 
 bool file_is_empty(const file *f) {
-   if(f == NULL) {
-      fprintf(stderr,"Pointeur invalide\n");
-      exit(1);
-   }
-   return f->first == NULL;
+    abort_file(f);
+    return f->first == NULL;
 }
 
-
 void file_push(file *f, unsigned int rayon) {
-   if(f == NULL) {
-      fprintf(stderr,"Pointeur invalide\n");
-      exit(1);
-   }
-   if(rayon < SCREEN_HEIGHT/2) {
-      fprintf(stderr,"L'obstacle ne commence pas a l'exterieur de l'ecran\n");
-      exit(1);
-   }
-   obstacle *new = obstacle_initialize(rayon);
+    abort_file(f);
+    if(rayon < SCREEN_HEIGHT/2) {
+        fprintf(stderr,"L'obstacle ne commence pas a l'exterieur de l'ecran\n");
+        exit(1);
+    }
+    obstacle *new = obstacle_initialize(rayon);
 
-   if(f->first == NULL){
-      f->first = f->last = new;
-   }
-   else {
-      new->next = f->last;
-      f->last->prev = new;
-      f->last = new;
-   }
+    if(f->first == NULL){
+        f->first = f->last = new;
+    }
+    else {
+        new->next = f->last;
+        f->last->prev = new;
+        f->last = new;
+    }
 }
 
 unsigned int file_pop(file *f) {
-   if(f == NULL) {
-      fprintf(stderr, "Pointeur invalide\n");
-      exit(1);
-   }
-   if(f->first == NULL) {
-      return 0;
-   }
-   obstacle *temp = f->first;
-   int cadran = temp->cadran;
-   if(f->first == f->last) {
-      f->first = f->last = NULL;
-      free(temp);
-   }
-   else{
-   f->first = temp->prev;
-   f->first->next = NULL;
-   free(temp);
-   }
-   return cadran;
-}
+    abort_file(f);
 
+    if(f->first == NULL) {
+      return 0; 
+    }
+    obstacle *temp = f->first;
+    int cadran = temp->cadran;
+    if(f->first == f->last) {
+        f->first = f->last = NULL;
+        free(temp);
+    }
+    else{
+        f->first = temp->prev;
+        f->first->next = NULL;
+        free(temp);
+    }
+    return cadran;
+}
 
 obstacle* obstacle_initialize(int rayon) {
    obstacle *new = malloc(sizeof(obstacle));
@@ -71,14 +61,12 @@ obstacle* obstacle_initialize(int rayon) {
 }
 
 void file_delete(file *f) {
-   if(f == NULL) {
-      fprintf(stderr, "Pointeur invalide\n");
-      exit(1);
-   }
-   while(file_pop(f) != 0);
+    abort_file(f);
+    while(file_pop(f) != 0);
 }
 
 void file_display(SDL_Renderer *renderer, const file *f) {
+    abort_file(f);
     obstacle *noeud_courant = f->last;
     while (noeud_courant != NULL){
     obstacle_display(renderer, noeud_courant);
@@ -91,15 +79,13 @@ int obstacle_display(SDL_Renderer *renderer,const obstacle *o) {
       fprintf(stderr, "Pointeur invalide");
       exit(1);
    }
-   if(o == NULL) {
-      fprintf(stderr, "Pointeur invalide");
-      exit(1);
-   }
+   abort_obstacle(o);
+
    int couleur = 255;
    int fin_cadran = (o->cadran*60) - 1;
- 
-    if (o->cadran == 1 || o->cadran == 3 || o->cadran == 5)
-      couleur = 230;
+
+   if (o->cadran == 1 || o->cadran == 3 || o->cadran == 5)
+       couleur = 230;
    
    int debut_cadran = fin_cadran - 59;
 
@@ -118,3 +104,16 @@ int obstacle_cadran() {
    return num_cadran;
 }
 
+void abort_file(const file *f) {
+    if(f == NULL) {
+        fprintf(stderr,"pointeur de file invalide");
+        exit(1);
+    }
+}
+
+void abort_obstacle(const obstacle *o) {
+    if(o == NULL) {
+        fprintf(stderr,"pointeur d'obstacle invalide");
+        exit(1);
+    }
+}
